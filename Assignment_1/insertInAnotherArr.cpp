57db5c3f6e979b124056a97b6ab0108d2539b384
asus test <msa-99@outlook.com>
2024-06-14 23:37:22 +0600

    insertArrayOnAnotherArray

--- Assignment_1/insertInAnotherArr.cpp
+++ Assignment_1/insertInAnotherArr.cpp
@@ -1,24 +1,24 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-// int main(){
-//     int firstArrSize;
-//     cin>>firstArrSize;
-//     vector<int> arr1(firstArrSize);
-//     for(int i=0; i<firstArrSize; i++){
-//         cin>>arr1[i];
-//      }
-//     int sexArraySize;
-//     cin>>sexArraySize;
-//    vector<int> arr2(sexArraySize);
-//      for(int i=0; i<sexArraySize; i++){
-//         cin>>arr2[i];
-//      }
-//      int insertIndex;
-//      cin>>insertIndex;
-//     arr1.insert(arr1.begin()+insertIndex, arr2.begin(), arr2.end());
-//     int len=arr1.size();
-//     for(int i=0; i<len; i++){
-//         cout<<arr1[i]<<" ";
-//     }
-//     return 0;
-// }
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int firstArrSize;
+    cin>>firstArrSize;
+    vector<int> arr1(firstArrSize);
+    for(int i=0; i<firstArrSize; i++){
+        cin>>arr1[i];
+     }
+    int sexArraySize;
+    cin>>sexArraySize;
+   vector<int> arr2(sexArraySize);
+     for(int i=0; i<sexArraySize; i++){
+        cin>>arr2[i];
+     }
+     int insertIndex;
+     cin>>insertIndex;
+    arr1.insert(arr1.begin()+insertIndex, arr2.begin(), arr2.end());
+    int len=arr1.size();
+    for(int i=0; i<len; i++){
+        cout<<arr1[i]<<" ";
+    }
+    return 0;
+}
